0ff9ec1955ac1b0a6df36f82e92350af7205ad28
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-05 12:01:32 +0300

    Начало 2 урока и его дефолтная установка

---
+++ src/RRD/Lesson_2/Collection_2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { Routes, Route, Link } from 'react-router-dom';
+import { Homepage } from './pages/Homepage';
+import { About } from './pages/Aboutpage';
+import { Blogpage } from './pages/Blogpage';
+import { Notfoundpage } from './pages/Notfoundpage';
+
+
+export function Collection_2() {
+    return (
+        <>
+            <header>
+                <Link to="/">Home</Link>
+                <Link to="/posts">Blog</Link>
+                <Link to="/about">About</Link>
+            </header>
+            
+            {/* note.md => Lesson #1*/}
+            <Routes>
+                <Route path="/" element={ <Homepage />}/>
+                <Route path="/about" element={ <About />}/>
+                <Route path="/posts" element={ <Blogpage />}/>
+                <Route path="*" element={ <Notfoundpage />}/>
+            </Routes>
+        </>
+    )
+}
