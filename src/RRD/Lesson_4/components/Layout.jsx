33b58e8897ece03352bfebc394f4113a2ad927c8
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-05 17:16:08 +0300

    Занятие №4 про параметры в ссылах

---
+++ src/RRD/Lesson_4/components/Layout.jsx
@@ -0,0 +1,24 @@
+import { NavLink, Outlet } from 'react-router-dom';
+import { CustomLink } from './Stylesheet/CustomLink';
+
+const setActive = (({isActive}) => ({color: isActive ? "var(--color-active)" : "white"}))
+
+const Layout = () => {
+  return (
+    <>
+      <header>
+        <CustomLink to="/" >Home</CustomLink>
+        <CustomLink to="/posts" >Blog</CustomLink>
+        <CustomLink to="/about" >About</CustomLink>
+      </header>
+
+      <div className="container"> 
+        <Outlet />  
+      </div>
+      
+      <footer className="container">2021</footer>
+    </>
+  );
+};
+
+export { Layout };
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { Collection_3 } from './RRD/Lesson_3/Collection_3';
+import { Collection_4 } from './RRD/Lesson_4/Collection_4';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter >
-      <Collection_3 />
+      <Collection_4 />
     </BrowserRouter >
   </React.StrictMode>
 );
