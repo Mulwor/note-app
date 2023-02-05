604a04421db3248939fa04cb77e7b4a0d11954ad
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-05 11:24:34 +0300

    базовая установка первого урока

---
+++ src/RRD/Lesson_1/Collection_1.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function Collection_1() {
+    return (
+        <>
+            <header>
+                <a href="/">Home</a>
+                <a href="/posts">Blog</a>
+                <a href="/about">About</a>
+            </header>
+            
+            <div>
+                <h1>Get started with RRD-VI </h1>
+            </div>
+        </>
+    )
+}
---
+++ src/RRD/note.md
@@ -0,0 +1,2 @@
+Ресур взят из следующих источников: 
+1. https://www.youtube.com/playlist?list=PLiZoB8JBsdznY1XwBcBhHL9L7S_shPGVE
