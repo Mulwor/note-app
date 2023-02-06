a64f94f75482fedd4e50b28f6b63348d6275290c
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-06 13:50:07 +0300

    теперь берем данные и оборачивам в useState, однако в будущем обрати внимание на обратные кавычки в фетче

--- src/RRD/Lesson_4/pages/Singlepage.jsx
+++ src/RRD/Lesson_4/pages/Singlepage.jsx
@@ -1,9 +1,23 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react';
 
 export const SinglePage = () => {
     const {id} = useParams();
+    const [post, setPost] = useState(null)
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+          .then((res) => res.json())
+          .then((data) => setPost(data));
+      }, [id]);
+    
 
     return (
-        <div>{id}</div>
+        <div>{post && (
+            <>
+                <h1>{post.title}</h1>
+                <p>{post.body}</p>
+            </>
+        )}</div>
     )
 }
