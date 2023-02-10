7c872727f22d50d59aa83a91966fbe8cc0d25509
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-10 16:27:07 +0300

    clear

--- src/RRD/Lesson_4/pages/CreatePost.jsx
+++
@@ -1,9 +0,0 @@
-const Createpost = () => {
-    return (
-        <div>
-            <h1>Create a post</h1>
-        </div>
-    )
-}
-
-export {Createpost}
--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { Collection_4 } from './RRD/Lesson_4/Collection_4';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter >
-      <Collection_4 />
+      <App />
     </BrowserRouter >
   </React.StrictMode>
 );
