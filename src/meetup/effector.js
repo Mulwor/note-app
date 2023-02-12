717d950d5adcbfba26b44aac33b979bc990080c3
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-12 19:48:52 +0300

    очистка комментариев

--- src/meetup/effector.js
+++ src/meetup/effector.js
@@ -1,34 +1,17 @@
 import { createEffect, createEvent, createStore } from "effector";
 import { wait } from "./components/helpers/wait";
 
-// 2. Способ управления нашим хранилищам.
 export const setModalVisibility = createEvent();
-
-// 8. Мы можем эффектор использовать когда хотим получить какие-то данные с бека.
-// Чтобы не путать евенты с эффектами в коде, добавляет слово Fx
 export const getDataFx = createEffect((number = 3) => {
   return fetch(
     `https://jsonplaceholder.typicode.com/todos?_start=0&_limit=${number}`
   ).then((response) => wait(3000, response.json()));
 })
 
-
-// 1. Cоздаем стор-хранилища, в котором будет хранится состояния видимости
-// нашего окна (то есть оно видимое или нет). Аргументом createStore принимает
-// значение по умолчанию, что значит, что оно скрыто (false)
 export const $modalVisibility = createStore(false)
-    // 3. Логика прописывается в самом хранилище. С помощью on мы будем следить
-    // за срабатыванием события (setModalVisibility) вторым аргументом данные метод
-    // принимает функцию, которая нам будет возвращать новое состояние. Так как старое
-    // состояние нам не интересно, мы просто его обозначим нижним подчеркиванием и новое 
-    // состояние isVisible будем возвращать наше хранилище
   .on(setModalVisibility, (_, isVisivle) => isVisivle)
-  // Обновит наше состояние (модальное окно закроется после того как данные были получены)
   .reset(getDataFx.doneData)
 
-// 9. Хранилища в котором будут находится наши данные. По умолчанию у нас пустой массив.
-// Объединяем наш стор и наше хранилище. Нас интересует состояние когда мы уже получили 
-// данные. И когда мы получим данные оно перезапишется
 export const $todos = createStore([]).on(getDataFx.doneData, (_, data) => data);
 
 export const $dataLoading = createStore(false)
--- src/meetup/path.js
+++ src/meetup/path.js
@@ -1,21 +1,11 @@
 import React from 'react';
-
-// 4. Позволяет обращаться к нашему хранилищу. Стоит отметить, что названия store начинается
-// со знака $, что является лучшей практикой
 import { useStore } from 'effector-react';
 import { $modalVisibility } from './effector';
-
-// 7. Отвечает за изменения состояния
 import { setModalVisibility } from './effector';
-
-// 10. 
 import { $todos, getDataFx, $dataLoading } from './effector';
 
 export function Path() {
-  // 5. Объявляем переменную
   const isVisible = useStore($modalVisibility);
-  
-  // 10.
   const isLoading = useStore($dataLoading)
   const data = useStore($todos)
 
@@ -23,18 +13,14 @@ export function Path() {
     <>
       <Button color="primary" 
               variant="contained" 
-              // 7ю По нажатию модальное окно открывается
               onClick={() => setModalVisibility(true)}>
                     Открыть модальное окно
       </Button>
 
       <DataListModal
-        // 6. Изначально было false, сейчас уже мы пишем isVisible. После нажатия на кнопку,
-        // оно не будем работать так как мы нигде не использовали на клик, чтобы что-то выводилось
         isVisible={isVisible}
         isLoading={isLoading}
         onSubmit={getDataFx}
-        // 7. По нажатию модальное окно закрывается
         onClose={setModalVisibility(false)}
       />
 
