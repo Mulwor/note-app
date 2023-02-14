1d9fd6e30c6851e0ca4a779180504e59bb946f42
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-02-14 21:44:12 +0300

    feat: СВОЙ ПУТЬ ИЗУЧЕНИЯ ЭФФЕКТОРА - №1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-// Пример №1
-import { SampleComp } from './2. documentation/example/Loader';
-// Пример №2
-import { Sequence } from './2. documentation/example/Sequence';
-// Пример №3
-import { Counter } from './4. react/counter';
-import { Effectors } from './4. react/effector';
-import { Forms } from './4. react/forms';
-
+import { Counter } from './5. mine/counter/counter';
 
 function App() {
-  return (
-      <Forms />
-  )
-};
+  <Counter />
+}
 
 export default App;
