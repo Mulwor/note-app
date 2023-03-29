e0ec0cfdb20a2e30ede8902a65e5a62afc0b9d71
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-30 00:53:32 +0300

    Урок: 5, кнопка назад и переадресация по страницам

--- src/RRD/Lesson_5/pages/data-transfer/Singlepage.jsx
+++ src/RRD/Lesson_5/pages/data-transfer/Singlepage.jsx
@@ -1,24 +1,35 @@
-import { useParams, Link } from "react-router-dom"
-import { useState, useEffect } from 'react';
+import { useParams, Link, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export const SinglePage = () => {
-    const {id} = useParams();
-    const [post, setPost] = useState(null)
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const [post, setPost] = useState(null);
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-          .then((res) => res.json())
-          .then((data) => setPost(data));
-      }, [id]);
-    
+  // * На главную страницу
+  const goHome = () => navigate('/', {replace: true})
+  // * На одну страницу назад, другими словами, хотим вернутся на предыдущию страницу
+  const goAnotherBack = () => navigate(-1);
 
-    return (
-        <div>{post && (
-            <>
-                <h1>{post.title}</h1>
-                <p>{post.body}</p>
-                <Link to={`/posts/${id}/edit`}>Edit this post</Link>
-            </>
-        )}</div>
-    )
-}
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((res) => res.json())
+      .then((data) => setPost(data));
+  }, [id]);
+
+  return (
+    <div>
+      {post && (
+        <>
+          {/* Good practice */}
+          <button onClick={goAnotherBack}>Назад на предыдущию страницу</button> <br></br>
+          {/* Bad practice */}
+          <button onClick={goHome}>Домой</button>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
+          <Link to={`/posts/${id}/edit`}>Edit this post</Link>
+        </>
+      )}
+    </div>
+  );
+};
