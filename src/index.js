4ca95e96b5fa90cce757600e6ea5b0551cd6c244
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-03-30 19:12:39 +0300

    Урок: 6 параметры поиска

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { Collection_5 } from './RRD/Lesson_5/Collection_5';
+import { Collection_6 } from './RRD/Lesson_6/Collection_6';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter >
-      <Collection_5 />
+      <Collection_6 />
     </BrowserRouter >
   </React.StrictMode>
 );
