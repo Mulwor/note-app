d19035bf6897841bb536b7510046f697566e6b77
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-04-05 21:53:17 +0300

    use hook useSelector() and take him state

--- src/Michael/With Redux/With.jsx
+++ src/Michael/With Redux/With.jsx
@@ -3,40 +3,25 @@ import TodoList from "./components/TodoList";
 import InputField from "./components/InputField";
 
 function WithRedux() {
-  const [todos, setTodos] = React.useState([]);
   const [text, setText] = React.useState("");
 
-  const addTodo = () => {
-    if (text.trim().length) {
-      setTodos([
-        ...todos,
-        {
-          id: new Date().toISOString(),
-          text,
-          completed: false,
-        },
-      ]);
-      setText("");
-    }
-  };
-
   const removeTodo = (todoId) => {
-    setTodos(todos.filter((todo) => todo.id !== todoId));
+    // setTodos(todos.filter((todo) => todo.id !== todoId));
   };
 
   const toggleTodoComplete = (todoId) => {
-    setTodos(
-      todos.map((todo) => {
-        if (todo.id !== todoId) return todo;
-        return { ...todo, completed: !todo.completed };
-      }),
-    );
+    // setTodos(
+    //   todos.map((todo) => {
+    //     if (todo.id !== todoId) return todo;
+    //     return { ...todo, completed: !todo.completed };
+    //   }),
+    // );
   };
 
   return (
     <div className="App">
       <InputField text={text} handleInput={setText} handleSubmit={addTodo} />
-      <TodoList todos={todos} toggleTodoComplete={toggleTodoComplete} removeTodo={removeTodo}/>
+      <TodoList  />
     </div>
   );
 }
