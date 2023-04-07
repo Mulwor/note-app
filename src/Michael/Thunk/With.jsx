4ff539b7f37a44a6bb7288bca149e70e982da071
Ali Adigezalli <91879193+Mulwor@users.noreply.github.com>
2023-04-07 22:02:09 +0300

    taking error mistake from Api

--- src/Michael/Thunk/With.jsx
+++ src/Michael/Thunk/With.jsx
@@ -1,25 +1,28 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTodo, fetchTodos } from "./store/todoSlice";
 import TodoList from "./components/TodoList";
 import InputField from "./components/InputField";
 
 function Thunk() {
   const [text, setText] = React.useState("");
+  const {status, error} = useSelector((state) => state.todos);
   const dispatch = useDispatch();
 
   const addTask = () => {
     dispatch(addTodo({ text }));
-    setText('');
+    setText("");
   };
 
   useEffect(() => {
-    dispatch(fetchTodos())
+    dispatch(fetchTodos());
   }, [dispatch]);
 
   return (
     <div className="App">
       <InputField text={text} handleInput={setText} handleSubmit={addTask} />
+      {status === 'loading' && <h2>Загрузка</h2>}
+      {error && <h2>Возникла ошибка: {error} </h2>}
       <TodoList />
     </div>
   );
--- src/Michael/Thunk/store/todoSlice.js
+++ src/Michael/Thunk/store/todoSlice.js
@@ -1,17 +1,24 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchTodos = createAsyncThunk("todos/fetchTodos", async function () {
-  const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
-  const data = await response.json();
-  return data;
+export const fetchTodos = createAsyncThunk("todos/fetchTodos", async function ( _, {rejectWithValue} ) {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10");
+    if (!response.ok) {
+      throw new Error('Server Error!')
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    return rejectWithValue(error.message)
+  }
 });
 
 const todoSlice = createSlice({
   name: "todos",
   initialState: {
     todos: [],
-    status: null, 
-    error: null
+    status: null,
+    error: null,
   },
   reducers: {
     addTodo(state, action) {
@@ -32,15 +39,18 @@ const todoSlice = createSlice({
     },
   },
   extraReducers: {
-    [fetchTodos.pending]:(state) => {
-      state.status = 'loading';
+    [fetchTodos.pending]: (state) => {
+      state.status = "loading";
       state.error = null;
     },
     [fetchTodos.fulfilled]: (state, action) => {
-      state.status = 'resolved';
+      state.status = "resolved";
       state.todos = action.payload;
     },
-    [fetchTodos.rejected]: (state, action) => {},
+    [fetchTodos.rejected]: (state, action) => {
+      state.status = 'rejected';
+      state.error = action.payload;
+    },
   },
 });
 
