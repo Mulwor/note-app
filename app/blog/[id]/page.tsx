b9cdf74598f76a48f83f0e45c2896cb9baaac9a9
Mulwor <adigezalli@yandex.ru>
2024-06-05 00:36:52 +0300

    feat: error and loading page, and added fetch

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,14 @@
 import { Metadata } from "next";
 
+async function getData(id: string) {
+  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    next: {
+      revalidate: 60          // ? С каким промежутком мы хотим делать запросы
+    }
+  })
+  return response.json()
+}
+
 type Props = {
   params: {
     id: string;
@@ -7,13 +16,20 @@ type Props = {
 }
 
 export async function generateMetadata({params: {id}}: Props): Promise<Metadata> {
+  const post = await getData(id)
+  
   return {
-    title: id,
+    title: post.title,
   }
 }
 
-export default function Post({params: {id}}: Props) {
+export default async function Post({params: {id}}: Props) {
+  const post = await getData(id)
+
   return (
-    <h1>Post page: {id}</h1>
+    <>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </>
   )
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,34 @@
 import { Metadata } from "next"
+import Link from "next/link"
+
+async function getData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
+    next: {
+      revalidate: 60          // ? С каким промежутком мы хотим делать запросы
+    }
+  })
+
+  // if (!response.ok) throw new Error("Unable to fetch props")
+  return response.json()
+}
 
 export const metadata: Metadata = {
   title: "Blog | Next App"
 }
 
-export default function Blog() {
+export default async function Blog() {
+  const posts = await getData()
+
   return (
-    <h1>Blog page</h1>
+    <>
+      <h1>Blog page</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
