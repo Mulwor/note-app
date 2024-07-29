17c745de29dd5858cf774f83a34c2596737ea842
Mulwor <adigezalli@yandex.ru>
2024-07-29 20:35:19 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import "./App.css"
 
-import SevenToTwelve from "./lessons/SecondToSix"
-
 function App() {
   return (
     <div className="App">
-      <SevenToTwelve />
+
     </div>
   )
 }
