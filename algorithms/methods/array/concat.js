737155ad882e9312861c489eea9c9b7a1d03248b
Mulwor <adigezalli@yandex.ru>
2025-03-16 20:25:36 +0300

    feat: old version

--- algorithms/methods/array/concat.js
+++
@@ -1,5 +0,0 @@
-// ! Array.prototype.concat() - создает новый массив, соединяя два предыдущих массива
-
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-array1.concat(array2);              // ? [a, b, c, d, e, f]
--- output/promise/methods/reject.js
+++
@@ -1,8 +0,0 @@
-// ? Метод Promise.reject(reason) используется для создания промиса, который сразу переходит
-// ? в состояние "отклонён" (rejected) с переданной причиной ошибки. 
-
-const rejectedPromise = Promise.reject('Что-то пошло не так');
-
-rejectedPromise
-  .then((value) =>  console.log('Успех:', value))
-  .catch((error) =>  console.log('Ошибка:', error));
