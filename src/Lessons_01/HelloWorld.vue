1ea3ba1d7c3e8be7e29494437f3505b19f5a7f19
Mulwor <adigezalli@yandex.ru>
2025-04-27 21:42:57 +0300

    create hello-world

---
+++ src/Lessons_01/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Hello world on VUE</h1>
+</template>
+
+<style scoped>
+h1 {
+  border: 1px solid orange;
+  padding: 20px;
+  font-size: 100px;
+  color: rgb(127, 127, 149);
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import './assets/main.css'
+
+import { createApp } from 'vue'
+import HelloWorld from './Lessons_01/HelloWorld.vue'
+
+createApp(HelloWorld).mount('#app')
