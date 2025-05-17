78d50976b5ef63dbf6085102140dc2c3a9609c8c
Mulwor <adigezalli@yandex.ru>
2025-05-17 15:51:15 +0300

    update

--- output/basic/data-types/data-types.js
+++ output/basic/data-types/data-types.js
@@ -1,4 +1,25 @@
+// ? Типы данных в JS
+
+let string = "Oleg";
+let number = 25;
+let bigInt = 1234567890123456789012345678901234567890n;
+let boolean = true;
+let id = Symbol("id");
+
+let undef;
+let emptyValue = null;
+
+console.log(undef); // undefined
+
+let user = { 
+  name: "Иван", 
+  age: 30 
+};
+
+// ! =================================================================
+
 // ? Определить тип данных можно через typeOf
+
 console.log(typeof 0)                               // * number
 console.log(typeof true)                            // * boolean
 console.log(typeof 'Javascript')                    // * string
