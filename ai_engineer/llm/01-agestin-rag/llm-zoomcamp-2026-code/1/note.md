6c5a6db8a3e0ecd5614bc8f3851bc2d35cc5c576
Mulwor <adigezalli@yandex.ru>
2026-09-08 16:56:26 +0300

    update

---
+++ ai_engineer/llm/01-agestin-rag/llm-zoomcamp-2026-code/1/note.md
@@ -0,0 +1,9 @@
+В данном курсе рассмотрены следующие темы
+
+- Introduction - What we'll build and why
+- Environment Setup - Python, uv, OpenAI API
+- What is RAG - Why LLMs need context, the RAG architecture
+- The Course FAQ Dataset - Fetching and exploring the FAQ data
+- Search - Building a search engine with minsearch
+- Building a Prompt - Combining search results into a prompt
+- RAG Pipeline - Wiring search + prompt + LLM together
---
+++ ai_engineer/llm/01-agestin-rag/llm-zoomcamp-2026-code/ingest.py
@@ -0,0 +1,35 @@
+import requests
+from minsearch import Index
+
+'''
+Функция для загрузки данных - состоит из нескольких блокнотов
+
+- [26] - Получение всех данных
+- [27] - Отправляем запрос на получения этих данных
+'''
+def load_faq_data():
+    docs_url = 'https://datatalks.club/faq/json/courses.json'
+    response = requests.get(docs_url)
+    courses_raw = response.json()
+
+    documents = []
+    url_prefix = 'https://datatalks.club/faq'
+
+    for course in courses_raw:
+      course_url = f'{url_prefix}{course["path"]}'
+      course_response = requests.get(course_url)
+      course_response.raise_for_status()
+      course_data = course_response.json()
+
+      documents.extend(course_data)
+
+    return documents
+
+# Все поля, которые можно использовать в качестве ключа
+def build_index(documents):
+    index = Index(
+        text_fields=['question', 'section', 'answer'],
+        keyword_fields=['course']
+    )
+    index.fit(documents)
+    return index
